88cab0f14db530f0c47da8a601fc470b7965034c
Dan Foley <dan@dan-foley.com>
2020-01-29 17:40:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,13 +3,16 @@
 class School
   
   attr_reader :school, :roster
+  attr_accessor :student
   
   def initialize(name)
     @name = name
     @roster = {}
   end
   
-  
+  def add_student(name, age)
+    @roster
+  end
   
   
   
